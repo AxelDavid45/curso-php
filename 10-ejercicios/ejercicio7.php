84752995142475346357bf75fd3be028cbfae32f
AxelDavid45 <axeldavid45@gmail.com>
2018-10-20 09:56:42 -0500

    Se finalizo la etapa de ejercicios del bloque 1

---
+++ 10-ejercicios/ejercicio7.php
@@ -0,0 +1,26 @@
+<?php
+/*Ejercicio 6: Hacer un programa que muestre todos los numeros entre dos numeros que nos vengan por la url*/
+if(isset($_GET['n1']) && isset($_GET['n2'])) {
+    $n1 = (int) $_GET['n1'];
+    $n2 = (int) $_GET['n2'];
+
+    $i = $n1 + 1;
+    if($n1 < $n2) {
+        for($i; $i < $n2; $i++) {
+            if($i%2 != 0) {
+                echo "<p>El numero $i es impar </p>";
+            } else {
+                echo "<p>El numero $i es par </p>";
+            }
+        }
+    } else {
+        echo '<h1>El numero 1 debe ser menor que el numero 2</h1>';
+    }
+
+} else {
+    echo '<h1>Escribe un parametro por la url</h1>';
+}
+
+
+
+?>
